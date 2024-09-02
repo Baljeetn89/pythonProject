a2bf15212df1a17133c92c62da05a3bbdfd9927a
Baljeet Kaur <Baljeet.NA@metropolia.fi>
2024-09-02 11:35:14 +0300

    Chp-3file1

---
+++ chp-3 file1.py
@@ -0,0 +1,8 @@
+height = float(input("Enter amount of money: "))
+if height >=178:
+    print("You are tall.")
+    if height <163:
+        print ("you are short .")
+
+        if(height <178) and (height >163):
+            print ("you are normal .")
